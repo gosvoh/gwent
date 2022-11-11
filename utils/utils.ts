1ba20766d5cf3f08d7323e625a908ca9473e4003
Aleksei Vokhmin <vohmina2011@yandex.ru>
2022-11-11 23:47:30 +0300

    Auth guard

---
+++ utils/utils.ts
@@ -0,0 +1,29 @@
+import { getSession } from "next-auth/react";
+
+export const requireAuth = async (context: any, callback: any) => {
+  const session = await getSession(context);
+
+  if (!session)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+
+  return callback({ session });
+};
+
+export const requireNonAuth = async (context: any, callback?: any) => {
+  const session = await getSession(context);
+
+  if (session)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+
+  return callback({ session });
+};
