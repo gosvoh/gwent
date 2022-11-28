33a583fe7a2c6a0597a862527e3ef86fb869215d
Aleksei Vokhmin <vohmina2011@yandex.ru>
2022-11-29 00:22:11 +0300

    New util function

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 import { getSession } from "next-auth/react";
 import post from "./request.manager";
+import { useCallback, useState } from "react";
 
 export const requireAuth = async (context: any, callback: any) => {
   const session = await getSession(context);
@@ -48,4 +49,54 @@ export async function getData<T>(
   if (data[0] && data[0].ERROR) res.status(401);
   else res.status(200);
   return data;
+}
+
+export type MapOrEntries<K, V> = Map<K, V> | [K, V][];
+
+// Public interface
+export interface Actions<K, V> {
+  set: (key: K, value: V) => void;
+  setAll: (entries: MapOrEntries<K, V>) => void;
+  remove: (key: K) => void;
+  reset: Map<K, V>["clear"];
+}
+
+// We hide some setters from the returned map to disable autocompletion
+type Return<K, V> = [
+  Omit<Map<K, V>, "set" | "clear" | "delete">,
+  Actions<K, V>
+];
+
+export function useMap<K, V>(
+  initialState: MapOrEntries<K, V> = new Map()
+): Return<K, V> {
+  const [map, setMap] = useState(new Map(initialState));
+
+  const actions: Actions<K, V> = {
+    set: useCallback((key, value) => {
+      setMap((prev) => {
+        const copy = new Map(prev);
+        copy.set(key, value);
+        return copy;
+      });
+    }, []),
+
+    setAll: useCallback((entries) => {
+      setMap(() => new Map(entries));
+    }, []),
+
+    remove: useCallback((key) => {
+      setMap((prev) => {
+        const copy = new Map(prev);
+        copy.delete(key);
+        return copy;
+      });
+    }, []),
+
+    reset: useCallback(() => {
+      setMap(() => new Map());
+    }, []),
+  };
+
+  return [map, actions];
 }
