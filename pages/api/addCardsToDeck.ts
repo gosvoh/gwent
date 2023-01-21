07f4aebd04a382c9caf418f32de2b006111375ea
Aleksei Vokhmin <vohmina2011@yandex.ru>
2023-01-21 10:31:29 +0300

    New API

---
+++ pages/api/addCardsToDeck.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getData } from "../../utils/utils";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") return;
+
+  const { cards } = JSON.parse(req.body);
+  const { gameId } = req.query as { gameId: string };
+  let result = await getData(
+    req,
+    res,
+    "addCardsToDeck",
+    gameId,
+    cards.join(",")
+  );
+  if (!result) return;
+  console.log("addCardsToDeck result", result);
+  res.json(result);
+  res.end();
+}
