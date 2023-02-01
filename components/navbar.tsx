3298e645bf7533cc4f6c86adfe8e7aeb36569641
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-02-01 15:08:55 +0300

    Fixed navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 import { signIn, useSession } from "next-auth/react";
 import styles from "../styles/Navbar.module.scss";
 import Logout from "./logout";
-import { Session } from "next-auth";
+import { getServerSession, Session } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
-export default function Navbar() {
-  const { data: session } = useSession();
-
+export default function Navbar({
+  authSession: session,
+}: {
+  authSession: Session | null;
+}) {
   return (
     <nav className={styles.navbar}>
       <Link href={"/"}>Home</Link>
@@ -36,4 +39,11 @@ function LoggedLayout({ session }: { session: Session }) {
       <p className={styles.player}>{session.user.name}</p>
     </>
   );
+}
+
+export async function getServerSideProps({ req, res }: any) {
+  let authSession = await getServerSession(req, res, authOptions);
+  return {
+    props: { authSession },
+  };
 }
